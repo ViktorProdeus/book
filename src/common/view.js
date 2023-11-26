3c597c7c0524ebfac16edc7b41d92b6f7f2b28d7
ViktorProdeus <viktorprodeus@gmail.com>
2023-11-26 18:35:43 +0300

    Первый компонент

--- src/common/view.js
+++ src/common/view.js
@@ -1,16 +1,17 @@
 export class AbstractView {
-    constructor() {
-        this.app = document.getElementById('root');
-    }
-    setTitle(title) {
-        document.title = title;
-    }
-    
-    render() {
-        return;
-    }
+	constructor() {
+		this.app = document.getElementById("root");
+	}
 
-    destroy() {
-        return;
-    }
+	setTitle(title) {
+		document.title = title;
+	}
+
+	render() {
+		return;
+	}
+
+	destroy() {
+		return;
+	}
 }
