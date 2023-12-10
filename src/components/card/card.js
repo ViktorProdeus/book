6b8a8a2d00999f229049ea94386a6332b9dcd261
ViktorProdeus <viktorprodeus@gmail.com>
2023-12-10 15:35:09 +0300

    Упражнение - Добавление в избранное

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -8,6 +8,16 @@ export class Card extends DivComponent {
 		this.cardState = cardState;
 	}
 
+	#addToFavorites() {
+		this.appState.favorites.push(this.cardState);
+	}
+
+	#deleteFromFavorites() {
+		this.appState.favorites = this.appState.favorites.filter(
+			b => b.key !== this.cardState.key
+		);
+	}
+
 	render() {
 		this.el.classList.add('card');
 		const existInFavorites = this.appState.favorites.find(
@@ -29,14 +39,23 @@ export class Card extends DivComponent {
 				</div>
 				<div class="card__footer">
 					<button class="button__add ${existInFavorites ? 'button__active' : ''}">
-						${existInFavorites 
+						${existInFavorites
 							? '<img src="/static/favorites.svg" />'
 							: '<img src="/static/favorites-white.svg" />'
 						}
 					</button>
 				</div>
 			</div>
-		`
+		`;
+		if (existInFavorites) {
+			this.el
+				.querySelector('button')
+				.addEventListener('click', this.#deleteFromFavorites.bind(this));
+		} else {
+			this.el
+				.querySelector('button')
+				.addEventListener('click', this.#addToFavorites.bind(this));
+		}
 		return this.el;
 	}
 }
